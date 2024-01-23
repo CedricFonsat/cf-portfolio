154e9a62e3c4b1b992d051e5bd3f815651c5a1a4
cedricfonsat <cedricfonsat@MacBook-Air-de-Cedric.local>
2024-01-23 21:42:20 +0100

    version 1.0.1

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,31 @@
-import { Asterisk } from 'lucide-react'
+import { Asterisk } from "lucide-react";
 
 const About = () => {
   return (
-    <section className="w-full h-96 bg-black mt-10 relative overflow-hidden flex flex-col items-center">
-    <div className="w-full flex items-center py-10">
-        <p className="text-white text-8xl font-bold">about</p>
-        <p><Asterisk size={70} color="white" /></p>
-        <p className="text-white text-8xl font-bold">about</p>
-        <p><Asterisk size={70} color="white" /></p>
-        <p className="text-white text-8xl font-bold">about</p>
-        <p><Asterisk size={70} color="white" /></p>
-        <p className="text-white text-8xl font-bold">about</p>
-    </div>
-    <div className="mt-10 md:w-1/2 px-4">
-        <p className="text-white text-center">Curious and attentive I am since my youngest age passionate by computer science and programming languages.</p>
-    </div>
-</section>
-  )
-}
+    <section id="about" className="w-full h-96 bg-black mt-10 relative overflow-hidden flex flex-col items-center">
+      <div className="w-full flex items-center py-10">
+        <p className="text-[#F8F7F3] text-8xl font-bold">about</p>
+        <p>
+          <Asterisk size={70} color="white" />
+        </p>
+        <p className="text-[#F8F7F3] text-8xl font-bold">about</p>
+        <p>
+          <Asterisk size={70} color="white" />
+        </p>
+        <p className="text-[#F8F7F3] text-8xl font-bold">about</p>
+        <p>
+          <Asterisk size={70} color="white" />
+        </p>
+        <p className="text-[#F8F7F3] text-8xl font-bold">about</p>
+      </div>
+      <div className="mt-10 md:w-1/2 px-4">
+        <p className="text-[#F8F7F3] text-center">
+          Curious and attentive I am since my youngest age passionate by
+          computer science and programming languages.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
---
+++ src/data/project.ts
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+        id: 1,
+        title: 'Embelir - Product advertising image generator',
+        typeProject: 'Personal Project',
+        date: '2023',
+        link: 'https://github.com/CedricFonsat/embelir',
+        publish: true
+    },
+    {
+        id: 2,
+        title: 'Collect7 - Marketplace NFT',
+        typeProject: 'School Project',
+        date: '2023',
+        link: 'https://github.com/CedricFonsat/col7_front',
+        publish: true
+    },
+    {
+        id: 3,
+        title: 'Pokedex',
+        typeProject: 'School Project',
+        date: '2022',
+        link: 'https://github.com/CedricFonsat/pokedex',
+        publish: true
+    },
+    {
+        id: 4,
+        title: 'PDF Devis Generator',
+        typeProject: 'Personal Project',
+        date: '2024',
+        link: 'https://github.com/CedricFonsat/devis-generator.git',
+        publish: true
+    }
+]
